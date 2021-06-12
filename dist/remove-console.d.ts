f7291ec93808cb063d6bbd0bd4363c097de19782
ticklepoke <lee.nigel@hotmail.com>
2021-06-12 10:39:31 +0800

    refactor(dist): Remove dist

--- dist/remove-console.d.ts
+++
@@ -1,5 +0,0 @@
-import { API, FileInfo, Options } from 'jscodeshift';
-export interface Context {
-    destructuredVariables: Set<string>;
-}
-export default function transform(file: FileInfo, api: API, options: Options): string;
