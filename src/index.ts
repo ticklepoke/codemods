c114d77b8266658e07a6e3506f57e1a875c62471
ticklepoke <lee.nigel@hotmail.com>
2021-06-12 11:30:58 +0800

    feat(terminal colors): Add terminal colors

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import colors from 'colors/safe';
+
 const TRANSFORMS = [
   {
     name: 'Remove console and console calls',
@@ -11,14 +13,17 @@ const TRANSFORMS = [
 ];
 
 function showOptions() {
-  console.log('Available transforms: \n');
+  console.log(colors.bgRed('\nAvailable transforms:\n'));
   for (const T of TRANSFORMS) {
-    console.log(`dist/${T.value}.js: \t ${T.name}`);
+    console.log(colors.blue(`dist/${T.value}.js:`), `\t ${T.name}`);
   }
-  console.log('\nUsage: jscodeshift -t dist/[TRANSFORM FILENAME.js] [YOUR FILE.js]');
+  console.log(colors.bgRed('\nUsage:\n'));
+  console.log(colors.yellow('\tjscodeshift -t dist/[TRANSFORM FILENAME.js] [YOUR FILE.js]'));
+  console.log(colors.bgRed('\nSetup:'));
   console.log(
-    '\nPlease install jscodeshift globally via npm due to a known bug with yarn (https://github.com/facebook/jscodeshift/issues/424): \n\n\tnpm install -g jscodeshift\n'
+    '\nPlease install jscodeshift globally via npm due to a known bug with yarn (https://github.com/facebook/jscodeshift/issues/424):'
   );
+  console.log(colors.yellow('\n\tnpm install -g jscodeshift\n'));
 }
 
 showOptions();
