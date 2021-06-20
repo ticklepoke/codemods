a8e1fb386b4e6a3e1e6d08ab4b4729878f3f167f
ticklepoke <lee.nigel@hotmail.com>
2021-06-20 16:17:25 +0800

    feat(convert-let-const): Implementation

---
+++ src/__testfixtures__/convert-let-const.output.js
@@ -0,0 +1,30 @@
+let a = 1;
+const b = 2;
+let c = 2;
+a = 3;
+{
+  const a = 2;
+  const b = 2;
+  c = 3;
+}
+{
+  const a = 2;
+  const b = 2;
+  const c = 2;
+}
+{
+  let d = 2;
+  {
+    d = 3;
+  }
+}
+function bar() {
+  let a = 1;
+  const b = 1;
+  a = 2;
+}
+const foo = () => {
+  let a = 1;
+  const b = 1;
+  a = 2;
+};
