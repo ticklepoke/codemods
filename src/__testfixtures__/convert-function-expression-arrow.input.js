ab20dd45b8b4a5b7dee9b0504a3697bf870bca03
ticklepoke <lee.nigel@hotmail.com>
2021-06-21 22:42:30 +0800

    test(convert-function-arrow): Add tests

---
+++ src/__testfixtures__/convert-function-expression-arrow.input.js
@@ -0,0 +1,7 @@
+const a = function () {};
+const b = function () {
+  this.a = 1;
+};
+const c = function () {
+  let a = 1;
+};
---
+++ src/__testfixtures__/convert-function-expression-arrow.output.js
@@ -0,0 +1,7 @@
+const a = () => {};
+const b = function () {
+  this.a = 1;
+};
+const c = () => {
+  let a = 1;
+};
