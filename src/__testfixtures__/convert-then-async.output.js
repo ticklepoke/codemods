849feac0651d08c13d8b870fd69cf6ba4529613d
ticklepoke <lee.nigel@hotmail.com>
2021-06-23 21:33:24 +0800

    test(then-async): Add tests

---
+++ src/__testfixtures__/convert-then-async.output.js
@@ -0,0 +1,46 @@
+async function a() {
+  const c = await b();
+  1 + 1;
+  return c;
+}
+
+async function b() {
+  const d = await c;
+  return d;
+}
+
+async function c() {
+  try {
+    const e = await d;
+    return e;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function d() {
+  try {
+    const f = await e;
+    return f;
+  } catch (err) {
+    console.error(err);
+  } finally {
+    1 + 1;
+    return 1;
+  }
+}
+
+async function e() {
+  const { a, b } = await f;
+  a + b;
+}
+
+const f = async function () {
+  const a = await g();
+  return a;
+};
+
+const g = async () => {
+  const a = await h();
+  return a;
+};
