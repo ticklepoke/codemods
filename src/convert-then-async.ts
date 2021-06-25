ba1771f6be1046db17af2fbee4d5982e346b8d3f
ticklepoke <lee.nigel@hotmail.com>
2021-06-25 20:20:54 +0800

    feat(then-async): Add support for catch block

--- src/convert-then-async.ts
+++ src/convert-then-async.ts
@@ -1,4 +1,4 @@
-import { API, FileInfo, Options } from 'jscodeshift';
+import { API, BlockStatement, FileInfo, JSCodeshift, MemberExpression, Options } from 'jscodeshift';
 import { applyMultipleTransforms } from './utils';
 
 /**
@@ -23,48 +23,11 @@ function transformFunctionDeclaration(props: { file: FileInfo; api: API; options
     .find(j.FunctionDeclaration)
     .forEach((path) => {
       const { body, id, params } = path.value;
-      const finalStatement = body.body[body.body.length - 1];
-      if (
-        finalStatement.type === 'ReturnStatement' &&
-        finalStatement.argument &&
-        finalStatement.argument.type === 'CallExpression' &&
-        finalStatement.argument.callee.type === 'MemberExpression' &&
-        finalStatement.argument.callee.property.type === 'Identifier'
-      ) {
-        const {
-          argument: {
-            callee: { property: finalProperty, object },
-            arguments: returnArgs,
-          },
-        } = finalStatement;
-
-        if (
-          finalProperty.name === 'then' &&
-          returnArgs.length === 1 &&
-          (returnArgs[0].type === 'FunctionExpression' || returnArgs[0].type === 'ArrowFunctionExpression')
-        ) {
-          // TODO: only assuming single then for now
-
-          body.body.pop();
-          // Add a variable declarator for the await
-          body.body.push(
-            j.variableDeclaration('const', [j.variableDeclarator(returnArgs[0].params[0], j.awaitExpression(object))])
-          );
-          // push the remaining then callback
-          if (returnArgs[0].body.type === 'BlockStatement') {
-            body.body.push(...returnArgs[0].body.body);
-          } else {
-            body.body.push(j.returnStatement(returnArgs[0].body));
-          }
-          // if callback returns single line, create a return statement
-          const newFn = j.functionDeclaration(id, params, body);
-          newFn.async = true;
-          j(path).replaceWith(newFn);
-        } else if (finalProperty.name === 'catch') {
-          console.log('TODO: catch');
-        } else if (finalProperty.name === 'finally') {
-          console.log('TODO: finally');
-        }
+      const newBody = createSingleThenBody(body, j);
+      if (newBody) {
+        const newFn = j.functionDeclaration(id, params, newBody);
+        newFn.async = true;
+        j(path).replaceWith(newFn);
       }
     })
     .toSource();
@@ -75,7 +38,18 @@ function transformFunctionExpression(props: { file: FileInfo; api: API; options:
 
   const j = api.jscodeshift;
 
-  return j(file.source).find(j.FunctionExpression).toSource();
+  return j(file.source)
+    .find(j.FunctionExpression)
+    .forEach((path) => {
+      const { body, id, params } = path.value;
+      const newBody = createSingleThenBody(body, j);
+      if (newBody) {
+        const newFn = j.functionExpression(id, params, newBody);
+        newFn.async = true;
+        j(path).replaceWith(newFn);
+      }
+    })
+    .toSource();
 }
 
 function transformArrowFunction(props: { file: FileInfo; api: API; options: Options }): string {
@@ -83,5 +57,91 @@ function transformArrowFunction(props: { file: FileInfo; api: API; options: Opti
 
   const j = api.jscodeshift;
 
-  return j(file.source).find(j.ArrowFunctionExpression).toSource();
+  return j(file.source)
+    .find(j.ArrowFunctionExpression)
+    .forEach((path) => {
+      const { body, params } = path.value;
+      if (body.type === 'BlockStatement') {
+        const newBody = createSingleThenBody(body, j);
+        if (newBody) {
+          const newFn = j.arrowFunctionExpression(params, newBody);
+          newFn.async = true;
+          j(path).replaceWith(newFn);
+        }
+      }
+    })
+    .toSource();
+}
+
+function createSingleThenBody(body: BlockStatement, j: JSCodeshift) {
+  const finalStatement = body.body[body.body.length - 1];
+  if (
+    finalStatement.type === 'ReturnStatement' &&
+    finalStatement.argument &&
+    finalStatement.argument.type === 'CallExpression' &&
+    finalStatement.argument.callee.type === 'MemberExpression' &&
+    finalStatement.argument.callee.property.type === 'Identifier'
+  ) {
+    const {
+      argument: {
+        callee: { property: finalProperty, object },
+        arguments: returnArgs,
+      },
+    } = finalStatement;
+
+    if (
+      returnArgs.length === 1 &&
+      (returnArgs[0].type === 'FunctionExpression' || returnArgs[0].type === 'ArrowFunctionExpression')
+    ) {
+      // TODO: only assuming single then for now
+      body.body.pop();
+      if (finalProperty.name === 'then') {
+        // Add a variable declarator for the await
+        body.body.push(
+          j.variableDeclaration('const', [j.variableDeclarator(returnArgs[0].params[0], j.awaitExpression(object))])
+        );
+        // push the remaining then callback
+        if (returnArgs[0].body.type === 'BlockStatement') {
+          body.body.push(...returnArgs[0].body.body);
+        } else {
+          body.body.push(j.returnStatement(returnArgs[0].body));
+        }
+
+        return body;
+      } else if (finalProperty.name === 'catch' && object.type === 'CallExpression') {
+        const finalArg = object.arguments[object.arguments.length - 1];
+        if (finalArg.type === 'FunctionExpression' || finalArg.type === 'ArrowFunctionExpression') {
+          const tryBlock = j.blockStatement([
+            j.variableDeclaration('const', [
+              j.variableDeclarator(finalArg.params[0], j.awaitExpression((object.callee as MemberExpression).object)),
+            ]),
+          ]);
+          if (finalArg.body.type === 'BlockStatement') {
+            tryBlock.body.push(...finalArg.body.body);
+          } else {
+            tryBlock.body.push(j.returnStatement(finalArg.body));
+          }
+
+          const catchBlock = j.blockStatement([]);
+          if (returnArgs[0].body.type === 'BlockStatement') {
+            catchBlock.body.push(...returnArgs[0].body.body);
+          } else {
+            catchBlock.body.push(j.returnStatement(returnArgs[0].body));
+          }
+          body.body.push(
+            j.tryStatement(
+              tryBlock,
+              j.catchClause(
+                // params
+                returnArgs[0].params[0],
+                null,
+                catchBlock
+              )
+            )
+          );
+          return body;
+        }
+      }
+    }
+  }
 }
