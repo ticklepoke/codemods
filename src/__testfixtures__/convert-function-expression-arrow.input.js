02ad1d8936a8ec4f3c51bd8b38028157d27ea69c
ticklepoke <lee.nigel@hotmail.com>
2021-06-25 08:13:46 +0800

    test(expression-arrow): Add generator and async test cases

--- src/__testfixtures__/convert-function-expression-arrow.input.js
+++ src/__testfixtures__/convert-function-expression-arrow.input.js
@@ -11,3 +11,29 @@ const d = function () {
 const e = function () {
   return this.a;
 };
+const aa = async function () {};
+const bb = async function () {
+  this.a = 1;
+};
+const cc = async function () {
+  let a = 1;
+};
+const dd = async function () {
+  return 1;
+};
+const ee = async function () {
+  return this.a;
+};
+const aaa = function* () {};
+const bbb = function* () {
+  this.a = 1;
+};
+const ccc = function* () {
+  let a = 1;
+};
+const ddd = function* () {
+  return 1;
+};
+const eee = function* () {
+  return this.a;
+};
