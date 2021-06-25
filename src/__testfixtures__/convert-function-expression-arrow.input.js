19b9df6b8dc3bca71dc6a97658254504e2164b72
ticklepoke <lee.nigel@hotmail.com>
2021-06-25 08:17:16 +0800

    refactor(expression-arrow): Refactor spacing

--- src/__testfixtures__/convert-function-expression-arrow.input.js
+++ src/__testfixtures__/convert-function-expression-arrow.input.js
@@ -1,39 +1,53 @@
 const a = function () {};
+
 const b = function () {
   this.a = 1;
 };
+
 const c = function () {
   let a = 1;
 };
+
 const d = function () {
   return 1;
 };
+
 const e = function () {
   return this.a;
 };
+
 const aa = async function () {};
+
 const bb = async function () {
   this.a = 1;
 };
+
 const cc = async function () {
   let a = 1;
 };
+
 const dd = async function () {
   return 1;
 };
+
 const ee = async function () {
   return this.a;
 };
+
 const aaa = function* () {};
+
 const bbb = function* () {
   this.a = 1;
 };
+
 const ccc = function* () {
   let a = 1;
 };
+
 const ddd = function* () {
   return 1;
 };
+
 const eee = function* () {
   return this.a;
 };
--- src/__testfixtures__/convert-function-expression-arrow.output.js
+++ src/__testfixtures__/convert-function-expression-arrow.output.js
@@ -1,35 +1,49 @@
 const a = () => {};
+
 const b = function () {
   this.a = 1;
 };
+
 const c = () => {
   let a = 1;
 };
+
 const d = () => 1;
+
 const e = function () {
   return this.a;
 };
+
 const aa = async () => {};
+
 const bb = async function () {
   this.a = 1;
 };
+
 const cc = async () => {
   let a = 1;
 };
+
 const dd = async () => 1;
+
 const ee = async function () {
   return this.a;
 };
+
 const aaa = function* () {};
+
 const bbb = function* () {
   this.a = 1;
 };
+
 const ccc = function* () {
   let a = 1;
 };
+
 const ddd = function* () {
   return 1;
 };
+
 const eee = function* () {
   return this.a;
 };
