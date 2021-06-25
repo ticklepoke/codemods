600c45ebd9aa565928d04f742ba77402e59e2eb8
ticklepoke <lee.nigel@hotmail.com>
2021-06-25 08:20:33 +0800

    test(bind-arrow): Add test cases

--- src/__testfixtures__/convert-bind-arrow-function.output.js
+++ src/__testfixtures__/convert-bind-arrow-function.output.js
@@ -47,3 +47,37 @@ const ddd = function* (a, b) {
   1 + 1;
   return 1;
 }.bind(that);
+
+const aa = async (a) => 1;
+
+const bb = async function (a) {
+  return 1;
+}.bind(that);
+
+const cc = async (a, b) => {
+  1 + 1;
+  return 1;
+};
+
+const dd = async function (a, b) {
+  1 + 1;
+  return 1;
+}.bind(that);
+
+const aaa = function* (a) {
+  return 1;
+}.bind(this);
+
+const bbb = function* (a) {
+  return 1;
+}.bind(that);
+
+const ccc = function* (a, b) {
+  1 + 1;
+  return 1;
+}.bind(this);
+
+const ddd = function* (a, b) {
+  1 + 1;
+  return 1;
+}.bind(that);
