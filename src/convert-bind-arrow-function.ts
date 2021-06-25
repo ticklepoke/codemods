bf80b45967f5ed8d212b4d467c69d12e65cb73d8
ticklepoke <lee.nigel@hotmail.com>
2021-06-25 08:20:46 +0800

    fix(bind-arrow): Add generator and async support

--- src/convert-bind-arrow-function.ts
+++ src/convert-bind-arrow-function.ts
@@ -16,7 +16,10 @@ export default function transform(file: FileInfo, api: API): string {
         args.length === 1 &&
         args[0].type === 'ThisExpression'
       ) {
-        const { body, params } = callee.object;
+        const { body, params, async: isAsync, generator: isGenerator } = callee.object;
+        if (isGenerator) {
+          return;
+        }
 
         const [newBody, useExpression] = ((): [BlockStatement | ExpressionKind, boolean] => {
           if (body.body.length === 1 && body.body[0].type === 'ReturnStatement' && body.body[0].argument) {
@@ -26,7 +29,9 @@ export default function transform(file: FileInfo, api: API): string {
           }
         })();
 
-        j(path).replaceWith(j.arrowFunctionExpression(params, newBody, useExpression));
+        const newFn = j.arrowFunctionExpression(params, newBody, useExpression);
+        newFn.async = isAsync;
+        j(path).replaceWith(newFn);
       }
     })
     .toSource({ arrowParensAlways: true });
