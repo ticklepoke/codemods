5c9aa2d41b2039d58f2c713d24a9e6eb955e8f5f
ticklepoke <lee.nigel@hotmail.com>
2021-06-26 22:06:34 +0800

    fix(bind-arrow): Remove dupe tests

--- src/__testfixtures__/convert-bind-arrow-function.input.js
+++ src/__testfixtures__/convert-bind-arrow-function.input.js
@@ -51,39 +51,3 @@ const ddd = function* (a, b) {
   1 + 1;
   return 1;
 }.bind(that);
-
-const aa = async function (a) {
-  return 1;
-}.bind(this);
-
-const bb = async function (a) {
-  return 1;
-}.bind(that);
-
-const cc = async function (a, b) {
-  1 + 1;
-  return 1;
-}.bind(this);
-
-const dd = async function (a, b) {
-  1 + 1;
-  return 1;
-}.bind(that);
-
-const aaa = function* (a) {
-  return 1;
-}.bind(this);
-
-const bbb = function* (a) {
-  return 1;
-}.bind(that);
-
-const ccc = function* (a, b) {
-  1 + 1;
-  return 1;
-}.bind(this);
-
-const ddd = function* (a, b) {
-  1 + 1;
-  return 1;
-}.bind(that);
